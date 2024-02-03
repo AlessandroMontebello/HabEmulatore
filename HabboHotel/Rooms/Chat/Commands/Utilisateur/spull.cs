96275cd794249bee7e9e31b81e394d045ad5cb0c
Alessandro Montebello <alessandro.montebello@simplenetworks.it>
2024-02-03 13:08:14 +0100

    test mac

--- HabboHotel/Rooms/Chat/Commands/Utilisateur/spull.cs
+++ HabboHotel/Rooms/Chat/Commands/Utilisateur/spull.cs
@@ -8,13 +8,13 @@ namespace Akiled.HabboHotel.Rooms.Chat.Commands.Cmd{    class spull : IChatCom
 
             if (Params.Length == 1)
             {
-                TargetUser.SendWhisperChat("Introduce el nombre del usuario que deseas hacer el super pull.");
+                TargetUser.SendWhisperChat("Introduce el nombre del usuario que deseas hacer el suuuper pull.");
                 return;
             }
 
             if (!Room.RoomData.SPullEnabled && !Room.CheckRights(Session, true) && !Session.GetHabbo().HasFuse("room_override_custom_config"))
             {
-                Session.SendWhisper("Oops, al parecer el dueño de la sala ha prohibido hacer los super pull en su sala.");
+                Session.SendWhisper("Oops, al parecer el dueï¿½o de la sala ha prohibido hacer los super pull en su sala.");
                 return;
             }
 
@@ -24,4 +24,4 @@ namespace Akiled.HabboHotel.Rooms.Chat.Commands.Cmd{    class spull : IChatCom
                 return;
             }
 
-            if (TargetUser == null)            {                TargetUser.SendWhisperChat(Convert.ToString(Params[1]) + " el usuario ya no se encuentra aquí.");                return;            }            if (TargetUser.GetClient().GetHabbo().Id == Session.GetHabbo().Id)                return;            if (TargetUser.GetClient().GetHabbo().PremiumProtect && !Session.GetHabbo().HasFuse("fuse_mod"))            {                roomuser.SendWhisperChat(AkiledEnvironment.GetLanguageManager().TryGetValue("premium.notallowed", Session.Langue));                return;            }            roomuser.OnChat("@red@ * Jalar Duro a " + Params[1] + "*,", 0, false);            if (roomuser.RotBody % 2 != 0)                roomuser.RotBody--;            if (roomuser.RotBody == 0)                TargetUser.MoveTo(roomuser.X, roomuser.Y - 1);            else if (roomuser.RotBody == 2)                TargetUser.MoveTo(roomuser.X + 1, roomuser.Y);            else if (roomuser.RotBody == 4)                TargetUser.MoveTo(roomuser.X, roomuser.Y + 1);            else if (roomuser.RotBody == 6)                TargetUser.MoveTo(roomuser.X - 1, roomuser.Y);        }    }}
+            if (TargetUser == null)            {                TargetUser.SendWhisperChat(Convert.ToString(Params[1]) + " el usuario ya no se encuentra aquï¿½.");                return;            }            if (TargetUser.GetClient().GetHabbo().Id == Session.GetHabbo().Id)                return;            if (TargetUser.GetClient().GetHabbo().PremiumProtect && !Session.GetHabbo().HasFuse("fuse_mod"))            {                roomuser.SendWhisperChat(AkiledEnvironment.GetLanguageManager().TryGetValue("premium.notallowed", Session.Langue));                return;            }            roomuser.OnChat("@red@ * Jalar Duro a " + Params[1] + "*,", 0, false);            if (roomuser.RotBody % 2 != 0)                roomuser.RotBody--;            if (roomuser.RotBody == 0)                TargetUser.MoveTo(roomuser.X, roomuser.Y - 1);            else if (roomuser.RotBody == 2)                TargetUser.MoveTo(roomuser.X + 1, roomuser.Y);            else if (roomuser.RotBody == 4)                TargetUser.MoveTo(roomuser.X, roomuser.Y + 1);            else if (roomuser.RotBody == 6)                TargetUser.MoveTo(roomuser.X - 1, roomuser.Y);        }    }}
